1ce6b1a432dc13b2adffaa52bf6201b53aef9a3f
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-09-26 20:19:35 -0600

    feat: agregar soporte para el título en los componentes AppLayout y GuestLayout

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -8,6 +8,15 @@ use Illuminate\View\View;
 class AppLayout extends Component
 {
     /**
+     * Create the component instance.
+     * 
+     * @param string $title
+     */
+    public function __construct(public string $title = 'Guest Layout')
+    {
+        //
+    }
+    /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -8,6 +8,16 @@ use Illuminate\View\View;
 class GuestLayout extends Component
 {
     /**
+     * Create the component instance.
+     * 
+     * @param string $title
+     */
+    public function __construct(public string $title = 'Guest Layout')
+    {
+        //
+    }
+
+    /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
